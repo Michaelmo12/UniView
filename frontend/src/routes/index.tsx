32ffb003f0ae8d940619f746f05e583b7888a3ff
Michael Mordehai <mordohmichael9@gmail.com>
2026-01-06 16:05:43 +0200

    Update routing for new pages
    - Add route for AddUser page
    - Remove SignIn route (merged with Login)
    - Update route configuration

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home.tsx";
 import Login from "../pages/Login.tsx";
-import SignIn from "../pages/SignIn.tsx";
+import AddUser from "../pages/AddUser.tsx";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/signin" element={<SignIn />} />
+      <Route path="/admin/add-user" element={<AddUser />} />
     </Routes>
   );
 }
