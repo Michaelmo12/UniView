2f8fa6a262c0c86a9c2afc4c1b6a95daf2e81b84
Michael Mordehai <mordohmichael9@gmail.com>
2026-01-13 14:27:01 +0200

    app routes

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { ProtectedRoute } from "../components/common";
 import Home from "../pages/Home.tsx";
 import Login from "../pages/Login.tsx";
 import AddUser from "../pages/AddUser.tsx";
@@ -6,9 +7,23 @@ import AddUser from "../pages/AddUser.tsx";
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
       <Route path="/login" element={<Login />} />
-      <Route path="/admin/add-user" element={<AddUser />} />
+      <Route
+        path="/admin/add-user"
+        element={
+          <ProtectedRoute adminOnly>
+            <AddUser />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
