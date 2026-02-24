236d23f136a1fb9ee483458a678465bfb033c550
Michael Mordehai <mordohmichael9@gmail.com>
2026-02-24 09:26:25 +0200

    settings.py security fix

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -12,8 +12,8 @@ class Settings:
     DB_HOST: str = os.getenv("DB_HOST", "localhost")
     DB_PORT: int = int(os.getenv("DB_PORT", 5432))
     DB_NAME: str = os.getenv("DB_NAME", "uniview_db")
-    DB_USER: str = os.getenv("DB_USER", "postgres")
-    DB_PASSWORD: str = os.getenv("DB_PASSWORD", "admin")
+    DB_USER: str = os.getenv("DB_USER")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD")
 
     # Service
     SERVICE_PORT: int = int(os.getenv("SERVICE_PORT", 8000))
